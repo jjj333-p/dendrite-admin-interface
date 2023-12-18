454cd29bb11bd90ad7ac59d35e996962a445a5fb
jjj333_p <jjj333.p.1325@gmail.com>
2023-12-17 14:45:00 -1000

    fix createroom, update dependency imports

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 //Import dependencies
 import { AutojoinRoomsMixin, MatrixClient, SimpleFsStorageProvider } from "matrix-bot-sdk"; 
-import { readFileSync } from "fs";
-import { parse } from "yaml";
+import fs from "fs";
+import yaml from "js-yaml";
 
 //Parse YAML configuration file
-const loginFile       = readFileSync('./db/login.yaml', 'utf8');
-const loginParsed     = parse(loginFile);
+const loginFile       = fs.readFileSync('./db/login.yaml', 'utf8');
+const loginParsed     = yaml.load(loginFile);
 const homeserver      = loginParsed["homeserver-url"];
 const accessToken     = loginParsed["login-token"];
 const adminRoom       = loginParsed["administration-room"];
@@ -23,64 +23,66 @@ let mxid;
 //Start Client
 client.start().then( async () => {
 
-    console.log("Client started!")
+  console.log("Client started!")
 
-    //get mxid
-    mxid = await client.getUserId()
+  //get mxid
+  mxid = await client.getUserId()
 
-    //if the feild is empty then we should create one
-    if ( !adminRoom ) {
+  //create pl feild for each authorized user
+  let authorizedPL = {[mxid]:101}
+  authorizedUsers.forEach(a => authorizedPL[a] = 100)
 
-        await client.createRoom({
-            initial_state:[
-                {
-                    content: {
-                      join_rule: "invite"
-                    },
-                    state_key: "",
-                    type: "m.room.join_rules"
-                },
-                {
-                    content: {
-                      "history_visibility": "joined"
-                    },
-                    state_key: "",
-                    type: "m.room.history_visibility"
-                },
-            ],
-            power_level_content_override:{
-                "ban": 50,
-                "events": {
-                  "m.room.avatar": 50,
-                  "m.room.canonical_alias": 50,
-                  "m.room.encryption": 100,
-                  "m.room.history_visibility": 100,
-                  "m.room.name": 50,
-                  "m.room.power_levels": 100,
-                  "m.room.server_acl": 100,
-                  "m.room.tombstone": 100
-                },
-                "events_default": 0,
-                "invite": 0,
-                "kick": 50,
-                "notifications": {
-                  "room": 50
-                },
-                "redact": 50,
-                "state_default": 50,
-                "users": {
-                  mxid: 101
-                },
-                "users_default": 0
-            },
-            invite:authorizedUsers,
-            is_direct:false,
-            name:"Administration Room"
-        }).catch(() => {})
+  //if the feild is empty then we should create a room
+  if ( !adminRoom ) {
 
-    }
+    await client.createRoom({
+        initial_state:[
+          {
+              content: {
+                join_rule: "invite"
+              },
+              state_key: "",
+              type: "m.room.join_rules"
+          },
+          {
+              content: {
+                "history_visibility": "joined"
+              },
+              state_key: "",
+              type: "m.room.history_visibility"
+          },
+        ],
+        power_level_content_override:{
+          "ban": 50,
+          "events": {
+            "m.room.avatar": 50,
+            "m.room.canonical_alias": 50,
+            "m.room.encryption": 100,
+            "m.room.history_visibility": 100,
+            "m.room.name": 50,
+            "m.room.power_levels": 100,
+            "m.room.server_acl": 100,
+            "m.room.tombstone": 100
+          },
+          "events_default": 0,
+          "invite": 0,
+          "kick": 50,
+          "notifications": {
+            "room": 50
+          },
+          "redact": 50,
+          "state_default": 50,
+          "users": authorizedPL,
+          "users_default": 0
+      },
+      invite:authorizedUsers,
+      is_direct:false,
+      name:"Administration Room"
+    })
 
-    //to remotely monitor how often the bot restarts, to spot issues
-    client.sendText(adminRoom, "Started.")
+  }
+
+  //to remotely monitor how often the bot restarts, to spot issues
+  // client.sendText(adminRoom, "Started.")
 
 })
